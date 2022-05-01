b42ad8ee35bbbe7f8b21d9863cfa7e38f904e9ee
Graciele Maria Ludwig <22711@student.dorset-college.ie>
2022-05-01 17:52:07 +0100

    First commit create a new activity added a button to go to next activity

---
+++ app/src/main/java/ie/dorset/student_22711/onlineshoppingapp/LoginActivity.kt
@@ -0,0 +1,10 @@
+package ie.dorset.student_22711.onlineshoppingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/ie/dorset/student_22711/onlineshoppingapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package ie.dorset.student_22711.onlineshoppingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //in build gradle is necessary to add android-kotlin-extensions for use kotlin synthetics
+        login_button.setOnClickListener {
+            Toast.makeText(this,"Button Clicked", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        // in case i want use Kotlin synthetics:
+      //  val loginButton = findViewById<Button>(R.id.login_button)
+      //  loginButton
+    }
+}
